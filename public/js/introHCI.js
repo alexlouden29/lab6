740de98b0e88694a3e2b2316cec8309a87dd8672
Alexander Louden <alouden@ucsd.edu>
2016-02-11 13:47:06 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,22 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	$.get("/project/"+idNumber, addDetails);
+	console.log("/project/"+idNumber);
+
+	//console.log("User clicked on project " + idNumber);
+}
+
+function addDetails(res){
+	//console.log(res);
+	var proj = "#project" + res.id;
+	var projdet = proj + " .details";
+	var image = res.image;
+	var summary = res.summary;
+	var title = res.title;
+	var date = res.date;
+
+	$(projdet).html('<img src="' + image + '"class="detailsImage"><h5>'+ title+ '</h5><h5>'+date+'</h5><p>'+summary+'</p>');
 }
 
 /*
@@ -35,4 +50,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", rando);
 }
+
+function rando(res){
+	var colors = res.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
+//<img src="res.image" class="detailsImage"><h3>res.date</h3><p>res.summary</p>
